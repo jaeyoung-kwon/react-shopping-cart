f90568e2cae8e75d53f0c39c4ef946248392f03c
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-05-29 15:07:15 +0900

    Updates

--- assets/index-C6wMAIk3.js
+++ assets/index-C6wMAIk3.js
@@ -12289,7 +12289,10 @@ const reset = css`
 `;
 async function enableMocking() {
   const { worker } = await __vitePreload(() => import("./browser-Cm04KqPi.js"), true ? [] : void 0);
-  return worker.start({
+  await worker.start({
+    serviceWorker: {
+      url: "/react-shopping-cart/mockServiceWorker.js"
+    },
     onUnhandledRequest: "bypass"
   });
 }
